969e09b84e16c9fb8a0a3f62d84828fe5ca1cf9a
tanvikad <tanvika.dasari@gmail.com>
2023-09-14 21:40:32 -0700

    client socket to my server

--- Makefile
+++ Makefile
@@ -5,6 +5,12 @@ output: lab1b_client.c lab1b_server.c
 # debug: lab1b.c
 # 	gcc -Wall -Wextra -g lab1b.c -o lab1b
 
+client:  lab1b_client.c
+	gcc -Wall -Wextra lab1b_client.c -o lab1b_client
+
+server: lab1b_server.c
+	gcc -Wall -Wextra lab1b_server.c -o lab1b_server
+
 clean:
 	rm -f *.o
 	rm -f lab1b_client
--- lab1b_client.c
+++ lab1b_client.c
@@ -9,6 +9,10 @@
 #include <poll.h>
 #include <termios.h>
 #include <sys/wait.h>
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <netdb.h> 
+
 
 
 
@@ -23,7 +27,7 @@ void set_current_terminal(struct termios * current_terminal) {
 
 
 //if it returns -1, the child should not be written to
-int input_read(int fd,  int shell_fd, int pid) {
+int input_read(int fd,  int shell_fd) {
     int size_to_read = 100;
     char buffer[size_to_read]; 
     int how_much_read = read(fd, buffer, size_to_read);
@@ -41,10 +45,7 @@ int input_read(int fd,  int shell_fd, int pid) {
                 return -2; 
             }
         }
-        if(buffer[i] == 3) {
-            kill(pid, SIGINT);
-            return 0;
-        }
+    
         if(buffer[i] == '\r' || buffer[i] == '\n') {
             if(fd == 0) {
                 //if we get input from the keyboard 
@@ -91,17 +92,23 @@ int main(int argc, char *argv[]){
         { 0, 0, 0, 0}
     };
 
-    char* name_of_program = NULL;
+    int port = -1;
     while((curr_option = getopt_long(argc, argv, "p", options, NULL)) != -1)  {
         switch(curr_option) {
             case 'p':
-                name_of_program = optarg;
+                // port = optarg;
+                sscanf(optarg, "%d", &port);
                 break;
             default:
                 fprintf(stderr, "Use the options --port. Getting error %s \n", strerror(errno));
                 exit(1);
                 break;
         }
+    }
+
+    if (port == -1) {
+        fprintf(stderr, "Port not specified \n");
+        exit(1);
     }
 
 
@@ -142,107 +149,90 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
+    int nfds = 1;
+    struct pollfd poll_fds[nfds];
+    poll_fds[0].fd = 0;
+    poll_fds[0].events = POLLIN;
+    //Add the socket into here
 
-    int pid = fork();
-    if (pid == -1) {
-        fprintf(stderr, "Forking failed due to %s\n", strerror(errno));
+    // This code was taken from cs.rpi 
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd < 0) {
+        fprintf(stderr, "ERROR opening socket due to erro %s \n", strerror(errno));
         exit(1);
     }
-    if(pid == 0) {
-
-        close(0);
-        close(1);
-        close(2);
-        dup(tToS_fd[0]);
-        dup(sToT_fd[1]);
-        dup(sToT_fd[1]);
-        close(tToS_fd[0]);
-        close(sToT_fd[1]);
-
-        //we have to close the ones we don't need
-        close(sToT_fd[0]); 
-        close(tToS_fd[1]);
-
-        if (name_of_program != NULL) {
-            printf("%s sad \n", name_of_program);
-            if(execl(name_of_program, name_of_program, NULL) == -1) {
-                fprintf(stderr, "Exec failed due to %s\n", strerror(errno));
-                exit(1);
-            }
-        }
-
-    }else {
-
-        close(tToS_fd[0]);
-        close(sToT_fd[1]);
-
-        int nfds = 2;
-        struct pollfd poll_fds[nfds];
-        if(name_of_program == NULL) {
-            nfds = 1;
-        }else {
-            poll_fds[1].fd = sToT_fd[0];
-            poll_fds[1].events = POLLIN;
-        }
-        poll_fds[0].fd = 0;
-        poll_fds[0].events = POLLIN;
-        
-        int keyboard_alive = 1;
-        int child_alive = 1; 
-        if(nfds == 1) child_alive = 0;
-        while(keyboard_alive || child_alive) {
-            
-            int ret = poll(poll_fds, nfds, -1);
-            if (ret <= 0) {
-                printf("Polling failed\r\n");
-                exit(1); 
-            }
-            for (int input_fd = 0; input_fd < nfds; input_fd++) {
-                if (poll_fds[input_fd].revents & POLLIN) {
-                    int read_return = input_read(poll_fds[input_fd].fd,  tToS_fd[1],  pid);
-                    if(read_return == -1) {
-                        close(tToS_fd[1]);
-                        keyboard_alive = 0;
-                        continue;
-
-                    } else if (read_return == -2 ) {
-                        //we got an EOF from the shell
-                        keyboard_alive = 0;
-                        child_alive = 0;
-                        continue;
-
-                    } else if (read_return == -3 && nfds == 2) {
-                        //we got a EPIPE
-                        keyboard_alive = 0;
-                        child_alive = 0;
-                        continue;
-                    }
-                }
-
-                if (poll_fds[input_fd].revents & POLLERR || poll_fds[input_fd].revents & POLLHUP) {
+    struct sockaddr_in serv_addr;
+    // char buffer[256];
+    struct hostent *server = gethostbyname("localhost");
 
 
-                    if(poll_fds[input_fd].fd != 0) {
-                        //the poll error happens on the child
-                        keyboard_alive = 0;
-                        child_alive = 0;
-                        break;
-                    } else {
-                        fprintf(stderr, "Polling failed on the keyboard %s\n", strerror(errno));
-                        exit(1);
-                    }
-                    poll_fds[input_fd].fd = -1;
-                }
-            }
-            
-        }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(port);
 
+    if (connect(socketfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)  {
+        fprintf(stderr, "ERROR accepting socket due to error %s \r\n", strerror(errno));
     }
+    
+    
+    int keyboard_alive = 1;
+    int child_alive = 1; 
+    if(nfds == 1) child_alive = 0;
+    while(keyboard_alive || child_alive) {
+        
+        int ret = poll(poll_fds, nfds, -1);
+        if (ret <= 0) {
+            printf("Polling failed\r\n");
+            exit(1); 
+        }
+        for (int input_fd = 0; input_fd < nfds; input_fd++) {
+            if (poll_fds[input_fd].revents & POLLIN) {
+                int read_return = input_read(poll_fds[input_fd].fd,  tToS_fd[1]);
+                if(read_return == -1) {
+                    close(tToS_fd[1]);
+                    keyboard_alive = 0;
+                    continue;
+
+                } else if (read_return == -2 ) {
+                    //we got an EOF from the shell
+                    keyboard_alive = 0;
+                    child_alive = 0;
+                    continue;
+
+                } else if (read_return == -3 && nfds == 2) {
+                    //we got a EPIPE
+                    keyboard_alive = 0;
+                    child_alive = 0;
+                    continue;
+                }
+            }
+
+            if (poll_fds[input_fd].revents & POLLERR || poll_fds[input_fd].revents & POLLHUP) {
 
 
-    int status;
-    waitpid(pid, &status, 0);
-    fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d \r\n", (status&0x7F), (status&0xff00)>>8);
+                if(poll_fds[input_fd].fd != 0) {
+                    //the poll error happens on the child
+                    keyboard_alive = 0;
+                    child_alive = 0;
+                    break;
+                } else {
+                    fprintf(stderr, "Polling failed on the keyboard %s\n", strerror(errno));
+                    exit(1);
+                }
+                poll_fds[input_fd].fd = -1;
+            }
+        }
+        
+    }
+    
+
+
+    // int status;
+    // waitpid(pid, &status, 0);
+    // fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d \r\n", (status&0x7F), (status&0xff00)>>8);
     set_current_terminal(&current_terminal);
 
     
